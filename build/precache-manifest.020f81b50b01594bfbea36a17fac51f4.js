cdb694ce2674ab47966300ba0b64e34db95408df
marcus finch <marcuswilliam0104@gmail.com>
2019-08-21 21:21:01 -0700

    BUILD

---
+++ build/precache-manifest.020f81b50b01594bfbea36a17fac51f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4bae7c5b002073ecdbfbd99716567d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2712afe0d6cc9c307c2",
+    "url": "/static/css/main.9b9fc507.chunk.css"
+  },
+  {
+    "revision": "b9b45e6c1af00619b279",
+    "url": "/static/js/2.9dd14434.chunk.js"
+  },
+  {
+    "revision": "e2712afe0d6cc9c307c2",
+    "url": "/static/js/main.7116abde.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "303cd5c4e2169130a9ddd90bfab76982",
+    "url": "/static/media/burger.303cd5c4.png"
+  }
+]);
